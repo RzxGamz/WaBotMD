1b66ef289324998bec8490c4f84da8f35f9682f3
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-23 23:28:53 +0700

    Update ytmp4.js

--- command/downloader/ytmp4.js
+++ command/downloader/ytmp4.js
@@ -7,9 +7,10 @@ module.exports = {
    desc: 'Download video youtube',
    use: '<link youtube>',
    async exec(msg, sock, args) {
-      if (!ytIdRegex.test(msg.body)) throw "Pastikan link yang kamu input adalah link youtube!"
+      if (!ytIdRegex.test(args[0])) throw "Pastikan link yang kamu input adalah link youtube!"
       try {
-        const res = await ytv(args.join(" "))
+        const resol = args[1] ? args[1] : "360p"
+        const res = await ytv(args[0], resol)
         const { dl_link, thumb, title, filesizeF, filesize } = res
         sock.sendFileFromUrl(msg.from, thumb, `Youtube Video\n\n${title}\n${filesize}`)
         sock.sendMessage(msg.from, { video: { url: dl_link } })
