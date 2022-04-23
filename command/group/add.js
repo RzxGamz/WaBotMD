917feacc845b6c7b6fb0c1f70e8cec13052b40f9
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-24 06:39:11 +0700

    Update add.js

--- command/group/add.js
+++ command/group/add.js
@@ -8,6 +8,8 @@ module.exports = {
    use: '<number phone>\nExample: /add 628xxxxx',
    async exec(msg, sock, args) {
       if (!msg.isGroup) throw djs.mess.group
+      if (!msg.isAdmins) throw djs.mess.admin
+      if (!msg.isBotAdmins) throw djs.mess.badmin
       if (args[0].startsWith("0")) throw "Masukkan kode negara"
       // id & people to add to the group (will throw error if it fails)
       const response = await sock.groupParticipantsUpdate(
