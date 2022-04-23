396d8e37ec068488f17df0b211929b2896e7d505
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-23 18:24:22 +0700

    Create owner.js

---
+++ command/main/owner.js
@@ -0,0 +1,23 @@
+module.exports = {
+   name: 'owner',
+   alias: ['creator','developer'],
+   category: 'main',
+   desc: 'Menampilkan contact owner bot',
+   async exec(msg, sock, args, arg) {
+      const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:RzxGamz\n' // full name
+            + 'ORG:Programmer;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=1 551 786 8060:+1 551 786 8060\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+      sock.sendMessage(
+      id,
+        { 
+           contacts: { 
+            displayName: 'Jeff', 
+            contacts: [{ vcard }] 
+           }
+        }
+      )
+   }
+}
