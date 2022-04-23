cfe63f85d7e09484b86f755aa7d17d1139faaf96
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-23 21:39:04 +0700

    Create add.js

---
+++ command/group/add.js
@@ -0,0 +1,20 @@
+const djs = require("@discordjs/collection")
+
+module.exports = {
+   name: 'add',
+   alias: ['oadd','addmember'],
+   category: 'group',
+   desc: 'Menambah member to group',
+   use: '<number phone>\nExample: /add 628xxxxx',
+   async exec(msg, sock, args) {
+      if (!msg.isGroup) throw djs.mess.group
+      if (args[0].startsWith("0")) throw "Masukkan kode negara"
+      // id & people to add to the group (will throw error if it fails)
+      const response = await sock.groupParticipantsUpdate(
+        msg.from, 
+        [args[0]+"@s.whatsapp.net"],
+        "add" // replace this parameter with "remove", "demote" or "promote"
+      )
+      msg.reply(response)
+   }
+}
