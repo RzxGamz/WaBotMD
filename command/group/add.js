37561e9dfdfe20b28f5689676f190bd857291485
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-24 09:15:27 +0700

    Update add.js

--- command/group/add.js
+++ command/group/add.js
@@ -7,16 +7,23 @@ module.exports = {
    desc: 'Menambah member to group',
    use: '<number phone>\nExample: /add 628xxxxx',
    async exec(msg, sock, args) {
-      if (!msg.isGroup) throw djs.mess.group
-      if (!msg.isAdmins) throw djs.mess.admin
-      if (!msg.isBotAdmins) throw djs.mess.badmin
+      if (!msg.isGroup) throw djs.mess_group
+      if (!msg.isAdmins) throw djs.mess_admin
+      if (!msg.isBotAdmins) throw djs.mess_badmin
       if (args[0].startsWith("0")) throw "Masukkan kode negara"
+      if (args[0]) {
       // id & people to add to the group (will throw error if it fails)
-      const response = await sock.groupParticipantsUpdate(
+      await sock.groupParticipantsUpdate(
         msg.from, 
         [args[0]+"@s.whatsapp.net"],
         "add" // replace this parameter with "remove", "demote" or "promote"
       )
-      msg.reply(response)
+      } else if (msg.quoted) {
+      await sock.groupParticipantsUpdate(
+        msg.from, 
+        [msg.quoted.participant],
+        "remove" // replace this parameter with "remove", "demote" or "promote"
+      )
+      }
    }
 }
