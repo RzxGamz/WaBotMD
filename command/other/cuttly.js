184cd65f0f9d6012cf8d020c6a69b6280ee69259
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-26 23:16:22 +0700

    Update cuttly.js

--- command/other/cuttly.js
+++ command/other/cuttly.js
@@ -1,3 +1,5 @@
+const { fetchJson } = require("../../lib/function/func")
+
 module.exports = {
    name: 'cuttly',
    alias: ['tocuttly','cutt.ly'],
@@ -7,7 +9,7 @@ module.exports = {
    async exec(msg, sock, args) {
      if (!args.join(' ') && !/^https?:\/\//.test(args.join(' '))) return msg.reply("Masukkan link!")
         try {
-          const res = require("../../lib/function/func").fetchJson(`https://cutt.ly/api/api.php?key=341578d2de946244680120edd9d03f068dd38&short=${args.join(' ')}`)
+          const res = await fetchJson(`https://cutt.ly/api/api.php?key=341578d2de946244680120edd9d03f068dd38&short=${args.join(' ')}`)
 	  msg.reply(JSON.stringify(res, null, 2))
 	} catch (e) {
 	    msg.reply(e.message)
