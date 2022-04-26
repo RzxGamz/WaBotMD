ce7e1cc870381fa43cddaf830647ad31bbaa58a5
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-26 20:30:17 +0700

    Update kick.js

--- command/group/kick.js
+++ command/group/kick.js
@@ -35,3 +35,11 @@ module.exports = {
       }
    }
 }
+
+const getGroupAdmins = (participantes) => {
+    var admins = []
+    for (let i of participantes) {
+        i.admin === "admin" ? admins.push(i.id) : ''
+    }
+    return admins
+}
