789c3321d80bb7dc5c37a8d0d47bf53f0c7eabb7
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-26 11:18:22 +0700

    Create pinterest.js

---
+++ command/search/pinterest.js
@@ -0,0 +1,19 @@
+const bs = require('@bochilteam/scraper')
+
+module.exports = {
+  name: 'pinterest',
+  alias: ['pin'],
+  category: 'search',
+  desc: 'Mencari gambar di pinterest',
+  use: '<query>',
+  async exec(msg, sock, args) {
+    if (!args.join(' ')) return msg.reply('Masukkan query!')
+    try {
+      const res = bs.pinterest(args.join(' '))
+      const result = res[Math.floor(Math.random() * res.length)]
+      sock.sendMessage(msg.from, { image: { url: result }, caption: result }, { quoted: msg })
+    } catch (e) {
+      msg.reply(e.message)
+    }
+  }
+}
