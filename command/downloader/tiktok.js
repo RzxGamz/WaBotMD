71cb6bfd619b67dc65062b2eab09d3f1e9677640
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-27 16:51:15 +0700

    Create tiktok.js

---
+++ command/downloader/tiktok.js
@@ -0,0 +1,29 @@
+const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+
+module.exports = {
+  name: 'tiktok',
+  alias: ['tiktokdl','ttdl'],
+  category: 'downloader',
+  desc: 'Download video tiktok',
+  use: '<link tiktok>\n\nDownload video :\n/tiktok <link> --video\n\nDownload audio :\n/tiktok <link> --audio',
+  async exec(msg, sock, args) {
+    if (!args[0].match(/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi)) return msg.reply('Masukkan link tiktok!')
+    try {
+      const opt = args[1]
+      const res = await tiktokdlv3(args[0])
+      const capt = JSON.stringify(res, null, 2)
+      switch (opt) {
+        case '--video':
+          await sock.sendFileFromUrl(msg.from, res.video.no_watermark, capt, msg)
+          break
+        case '--audio':
+          await sock.sendMessage(msg.from, { audio: { url: res.music }, mimetype: 'audio/mp4' }, { quoted: msg })
+          break
+        default:
+          await sock.sendFileFromUrl(msg.from, res.video.no_watermark, capt, msg)
+      }
+    } catch (e) {
+      msg.reply(e.message)
+    }
+  }
+}
