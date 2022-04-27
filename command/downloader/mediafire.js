a64af15cd9bdba975044b0cfc0cf960b9ffff36f
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-28 04:56:09 +0700

    Create mediafire.js

---
+++ command/downloader/mediafire.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const mime = require('mime-types')
+const cheerio = require('cheerio')
+
+module.exports = {
+   name: 'mediafire',
+   alias: ['mediafiredl'],
+   category: 'downloader',
+   desc: 'Download file mediafire',
+   use: '<link mediafire>',
+   async exec(msg, sock, args) {
+	if (!args[0]) return msg.reply('Masukkan link mediafire!')
+	mediafireDl(args[0]).then(async (res) => {
+		await msg.replyAd(JSON.stringify(res, null, 2), 'Mediafire Downloader', 'Download Mediafire on WhatsApp')
+		let mimetype = await mime.lookup(res.link)
+		await sock.sendMessage(msg.from, { document: { url: res.link }, filename: res.title, mimetype }, { quoted: msg })
+	}).catch(e => msg.reply(String(e)))
+   }
+}
+
+function mediafireDl(url) {
+	return new Promise((res, rej) => {
+		if (!/https?:\/\//.test(url) && !url.includes('mediafire')) return rej('Invalid url!')
+		axios(url).then(c => {
+			let $ = cheerio.load(c.data)
+			let title = $('div.dl-btn-label').attr('title')
+			let size = $('a#downloadButton').text().split('\n')[1].replace(/ /g, '').replace(/\(|\)/g, '').replace('Download', '')
+			let link = $('a#downloadButton').attr('href')
+			res({ title, size, link })
+		}).catch(rej)
+	})
+}
