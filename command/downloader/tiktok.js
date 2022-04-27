18aaa66f26bc4ca9088861523571610d33a0fc46
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-27 17:19:07 +0700

    Update tiktok.js

--- command/downloader/tiktok.js
+++ command/downloader/tiktok.js
@@ -12,16 +12,21 @@ module.exports = {
     try {
       await msg.reply('*Loading . . .*')
       const opt = args[1]
-      const res = await tiktokdlv3(args[0])
-      const capt = JSON.stringify(res, null, 2)
+      let res
+      let capt
       switch (opt) {
         case '--video':
+          res = await tiktokdlv3(args[0])
+          capt = JSON.stringify(res, null, 2)
           await sock.sendFileFromUrl(msg.from, res.video.no_watermark, capt, msg)
           break
         case '--audio':
+          res = await tiktokdlv3(args[0])
           await sock.sendMessage(msg.from, { audio: { url: res.music }, mimetype: 'audio/mp4' }, { quoted: msg })
           break
         default:
+          res = await tiktokdlv3(args[0])
+          capt = JSON.stringify(res, null, 2)
           await sock.sendFileFromUrl(msg.from, res.video.no_watermark, capt, msg)
       }
     } catch (e) {
