42e4250984085b00dc9e7a133ef1c4c9da50c767
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-28 22:53:11 +0700

    Update instagram.js

--- command/downloader/instagram.js
+++ command/downloader/instagram.js
@@ -12,7 +12,7 @@ module.exports = {
       await msg.reply('*Loading . . .*')
       const res = await igdl(args[0])
       const capt = JSON.stringify(res.user, null, 2)
-      for (let i = 0; i < res.medias.length; i++) await sock.sendFileFromUrl(msg.from, res.medias[i].downloadUrl, capt, msg)
+      for (let i = 0; i < res.medias.length; i++) await sock.sendFileFromUrl(msg.from, res.medias[i].url, capt, msg)
     } catch (e) {
       msg.reply(String(e))
     }
