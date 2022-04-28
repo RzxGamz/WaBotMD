68c5d71ff52ad850ccf45e36b658cab823fec623
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-28 22:46:48 +0700

    Update instagram.js

--- command/downloader/instagram.js
+++ command/downloader/instagram.js
@@ -7,11 +7,10 @@ module.exports = {
   desc: 'Download video or photo instagram',
   use: '<link instagram>',
   async exec(msg, sock, args) {
-    const url = args[0]
-    if (!url.includes('instagram')) return msg.reply('Masukkan link instagram!')
+    if (!args[0]) return msg.reply('Masukkan link instagram!')
     try {
       await msg.reply('*Loading . . .*')
-      const res = await igdl(url)
+      const res = await igdl(args[0])
       const capt = JSON.stringify(res.user, null, 2)
       for (let i = 0; i < res.medias.length; i++) await sock.sendFileFromUrl(msg.from, res.medias[i].downloadUrl, capt, msg)
     } catch (e) {
