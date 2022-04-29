acb471299724fdd8e71d0ef2e83ed1f5493e49fd
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-29 12:21:02 +0700

    Create self.js

---
+++ command/owner/self.js
@@ -0,0 +1,12 @@
+const djs = require("@discordjs/collection")
+
+module.exports = {
+   name: 'self',
+   desc: 'Mengubah mode ke self (khusus owner)',
+   category: 'owner',
+   async exec(msg, sock, args) {
+      if (djs.mode === "self") return msg.reply('Already in self mode!')
+      djs.mode = "self"
+      msg.replyAd('Sukses mengubah mode ke self', 'Self Mode', 'Bot sekarang hanya bisa di gunakan untuk owner')
+   }
+} 
