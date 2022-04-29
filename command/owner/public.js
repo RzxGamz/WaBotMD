ce252fd436e7f79d0ad80c45d48b03973a15e071
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-04-29 12:23:19 +0700

    Create public.js

---
+++ command/owner/public.js
@@ -0,0 +1,12 @@
+const djs = require("@discordjs/collection")
+
+module.exports = {
+   name: 'public',
+   desc: 'Mengubah mode ke public (untuk semua user)',
+   category: 'owner',
+   async exec(msg, sock, args) {
+      if (djs.mode === "public") return msg.reply('Already in public mode!')
+      djs.mode = "public"
+      msg.replyAd('Sukses mengubah mode ke public', 'Public Mode', 'Bot sekarang bisa di gunakan untuk semua user')
+   }
+} 
