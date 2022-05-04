a72b050751d7914ada4329fec6d6cb6cbcbfb6eb
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-05-05 00:01:37 +0700

    Update ytmp3.js

--- command/downloader/ytmp3.js
+++ command/downloader/ytmp3.js
@@ -13,7 +13,7 @@ module.exports = {
         const resol = args[1] ? args[1] : "128kbps"
         const res = await yta(args[0], resol)
         const { dl_link, thumb, title, filesizeF, filesize } = res
-        sock.sendFileFromUrl(msg.from, thumb, `Youtube Audio\n\n${JSON.stringify(res, null, 2)}`, msg)
+        sock.sendFileFromUrl(msg.from, thumb, `*YOUTUBE AUDIO*\n\n${String.fromCharCode(8206).repeat(4001)}${JSON.stringify(res, null, 2)}`, msg)
         sock.sendMessage(
         msg.from, 
         { audio: { url: dl_link }, mimetype: 'audio/mp4', contextInfo: { externalAdReply: { title: title, body: 'Play audio youtube', thumbnailUrl: thumb, mediaType: 2, mediaUrl: args[0] }}}
