516b8c3be6e33e8474322557828029911ad3cc9a
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-05-08 15:57:32 +0700

    Create white-gold-3d.js

---
+++ command/textpro/white-gold-3d.js
@@ -0,0 +1,13 @@
+const textpro = require('../../lib/scrape/textpro')
+
+module.exports = {
+   name: 'whitegold3d',
+   category: 'textpro',
+   desc: 'Membuat teks ke image',
+   use: '<text>',
+   async exec(msg, sock, args) {
+      if (!args[0]) return msg.reply('Masukkan text!')
+      const res = await textpro("https://textpro.me/elegant-white-gold-3d-text-effect-online-free-1070.html", `${args[0]}`)
+      await sock.sendMessage(msg.from, { image: { url: res }, caption: "*TEXT PRO - WHITE GOLD 3D*" }, { quoted: msg })
+   }
+}
