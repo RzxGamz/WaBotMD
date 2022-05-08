07b6157642ab7a1b042e8beebb4a22d41bdb9138
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-05-08 20:37:28 +0700

    Update light-glow.js

--- command/textpro/light-glow.js
+++ command/textpro/light-glow.js
@@ -6,8 +6,8 @@ module.exports = {
    desc: 'Membuat teks ke image',
    use: '<text>',
    async exec(msg, sock, args) {
-      if (!args[0]) return msg.reply('Masukkan text!')
-      const res = await textpro("https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html", `${args[0]}`)
+      if (!args.join(' ')) return msg.reply('Masukkan text!')
+      const res = await textpro("https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html", `${args.join(' ')}`)
       await sock.sendMessage(msg.from, { image: { url: res }, caption: "*TEXT PRO - LIGHT GLOW*" }, { quoted: msg })
    }
 }
