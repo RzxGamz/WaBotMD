5cb8edde0f6bc542bd49409780abf8dede9b4bb0
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-06-19 08:24:51 +0700

    Create topup_ff.js

---
+++ command/games/topup_ff.js
@@ -0,0 +1,17 @@
+const hikki = require('hikki-me')
+
+module.exports = {
+   name: 'topup_ff',
+   category: 'games',
+   use: '<diamond> <id ff>',
+   async exec(msg, sock) {
+      async function topupFF(dm, id) {
+         const num = msg.sender.split("@")[0]
+         const makeSession = await hikki.game.topupFreeFire(id, dm) // support nominal 5 12 70 140 355 720'
+         // console.log(makeSession) if get more property
+         return await hikki.game.payDiamond(makeSession, msg.sender.startsWith('62') ? "0"+num.slice(2) : num)
+      }
+      topupFF(args[0], args[1]).then(res => {
+      sock.sendMessage(from, { image: { url: res.qrCode }, caption: `*Top Up Free Fire*\n\nId : ${args[1]}\nDiamond : ${args[0]}\nPayment : QRIS\nTimer : 30 s\n\n_Silahkan scan qr code di atas untuk pembayaran diamond free fire_`}).catch(console.error)
+   }
+}
