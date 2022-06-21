e3ae7bde9cd5958859f8e74decba2c29ed4559f1
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-06-21 08:11:42 +0700

    Update play.js

--- command/downloader/play.js
+++ command/downloader/play.js
@@ -12,8 +12,8 @@ module.exports = {
       if (!args.join(' ')) return msg.reply('Masukkan judul video yang ingin kamu download!')
       try {
          const res = await bs.youtubeSearch(args.join(' '))
-         const buff = await getBuffer(res.all[0].image)
-         const template = baileys.generateWAMessageFromContent(msg.from, baileys.proto.Message.fromObject({ templateMessage: { hydratedTemplate: { hydratedContentText: `*YouTube Play*\n\nTitle: ${res.all[0].title}\nChannel: ${res.all[0].author.name}\nDuration: ${res.all[0].timestamp}\nView: ${res.all[0].views}\nUpload: ${res.all[0].ago}\nUrl: ${res.all[0].url}\nDescription: ${res.all[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, locationMessage: { jpegThumbnail: buff }, hydratedFooterText: "Rzx Bot", hydratedButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.all[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.all[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.all[0].url}` }}] } } }), { userJid: msg.sender, quoted: msg })
+         const buff = await getBuffer(res.video[0].thumbnail)
+         const template = baileys.generateWAMessageFromContent(msg.from, baileys.proto.Message.fromObject({ templateMessage: { hydratedTemplate: { hydratedContentText: `*YouTube Play*\n\nTitle: ${res.video[0].title}\nChannel: ${res.video[0].authorName}\nDuration: ${res.video[0].durationH}\nView: ${res.video[0].viewH}\nUpload: ${res.video[0].publishedTime}\nUrl: ${res.video[0].url}\nDescription: ${res.video[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, locationMessage: { jpegThumbnail: buff }, hydratedFooterText: "Rzx Bot", hydratedButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.all[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.all[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.all[0].url}` }}] } } }), { userJid: msg.sender, quoted: msg })
          sock.relayMessage(msg.from, template.message, { messageId: template.key.id } )
       } catch (e) {
        console.log(e)
