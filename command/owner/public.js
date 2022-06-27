60fe9fbbfcdb8fca51fe3120cfac36bedc0dff10
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-06-27 09:21:16 +0700

    Update public.js

--- command/owner/public.js
+++ command/owner/public.js
@@ -5,7 +5,7 @@ module.exports = {
    desc: 'Mengubah mode ke public (untuk semua user)',
    category: 'owner',
    async exec(msg, sock, args) {
-      if (!msg.isPrivate) return msg.reply('Khusus owner!')
+      if (!msg.key.fromMe) return msg.reply('Khusus owner!')
       if (djs.mode === "public") return msg.reply('Already in public mode!')
       djs.mode = "public"
       msg.replyAd('Sukses mengubah mode ke public', 'Public Mode', 'Bot sekarang bisa di gunakan untuk semua user')
