70dc288b63f906fecfff80d164b2644a79281651
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-06-27 08:27:14 +0700

    Update self.js

--- command/owner/self.js
+++ command/owner/self.js
@@ -5,6 +5,7 @@ module.exports = {
    desc: 'Mengubah mode ke self (khusus owner)',
    category: 'owner',
    async exec(msg, sock, args) {
+      if (!msg.isPrivate) return msg.reply('Khusus owner!')
       if (djs.mode === "self") return msg.reply('Already in self mode!')
       djs.mode = "self"
       msg.replyAd('Sukses mengubah mode ke self', 'Self Mode', 'Bot sekarang hanya bisa di gunakan untuk owner')
