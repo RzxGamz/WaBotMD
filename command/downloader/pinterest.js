e7318bfa8c4e02d4af3c997572f498dce64f9a3c
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-10-04 19:41:56 +0700

    Update pinterest.js

--- command/downloader/pinterest.js
+++ command/downloader/pinterest.js
@@ -2,12 +2,13 @@ const { fetchJson } = require('../../lib/function/func.js)
 
 module.exports = {
    name: 'pinterestdl',
+   alias: ['pindl'],
    category: 'downloader',
    desc: 'Download media pinterest',
    use: '<link pinterest>',
-   async exec(msg, sock) {
+   async exec(msg, sock, args) {
       msg.reply("Loading. . .")
-      let pin = await fetchJson(`https://api.zekais.com/pinterestdl?url=${msg.body.slice(12)}&apikey=zekais`)
+      let pin = await fetchJson(`https://api.zekais.com/pinterestdl?url=${args[0]}&apikey=zekais`)
       if (pin.type === "image") {
           sock.sendMessage(msg.from, { image: { url: pin.result }, caption: pin.result }, { quoted: msg })
       } else if (pin.type === "video") {
