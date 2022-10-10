477c46fe935aa42b9c37ee506b1ef51b202191f1
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-10-10 14:25:35 +0700

    Update help.js

--- command/main/help.js
+++ command/main/help.js
@@ -99,7 +99,7 @@ module.exports = {
      }}
            })*/
            const timez = moment().tz('Asia/Jakarta').format('HH:mm:ss')
-           if (timez < "04:00:00") {
+           /*if (timez < "04:00:00") {
                global.bupper = "https://i.ibb.co/PTzjXbY/malam.jpg"
            }
            if (timez < "10:00:00") {
@@ -113,8 +113,8 @@ module.exports = {
            } 
            if (timez < "23:59:00") {
                global.bupper = "https://i.ibb.co/PTzjXbY/malam.jpg"
-           }
-           const buffer = await reSize(global.bupper, 200, 200)
+           }*/
+           const buffer = await reSize(fs.readFileSync('././lib/media/pagi.jpg'), 200, 200)
            // Location Message
            //await sock.sendMessage(msg.from, { caption: str, footer: "Rzx Bot", location: { jpegThumbnail: buffer }, buttons: [{ buttonId: ".script", buttonText: { displayText: "Source Code" }, type: 1 }], headerType: 'LOCATION', mentions: [msg.sender] })
            // Location Message
