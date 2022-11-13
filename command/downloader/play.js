53ecf447e3dc2ecc48098052c3074a86472c2853
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-11-13 08:25:26 +0700

    Update play.js

--- command/downloader/play.js
+++ command/downloader/play.js
@@ -1,6 +1,4 @@
-const baileys = require('@adiwajshing/baileys')
 const bs = require('@bochilteam/scraper')
-const { getBuffer } = require('../../lib/function/func.js')
 
 module.exports = {
    name: 'play',
@@ -12,9 +10,7 @@ module.exports = {
       if (!args.join(' ')) return msg.reply('Masukkan judul video yang ingin kamu download!')
       try {
          const res = await bs.youtubeSearch(args.join(' '))
-         const buff = await getBuffer(res.video[0].thumbnail)
-         const bff1 = res.video[0].thumbnail
-         sock.sendMessage(msg.from, { text: `*YouTube Play*\n\nTitle: ${res.video[0].title}\nChannel: ${res.video[0].authorName}\nDuration: ${res.video[0].durationH}\nView: ${res.video[0].viewH}\nUpload: ${res.video[0].publishedTime}\nUrl: ${res.video[0].url}\nDescription: ${res.video[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, imageMessage: { url: bff1 }, footer: "Rzx Bot", templateButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.video[0].url}` }}] }, { quoted: msg })
+         await sock.sendMessage(msg.from, { text: `*YouTube Play*\n\nTitle: ${res.video[0].title}\nChannel: ${res.video[0].authorName}\nDuration: ${res.video[0].durationH}\nView: ${res.video[0].viewH}\nUpload: ${res.video[0].publishedTime}\nUrl: ${res.video[0].url}\nDescription: ${res.video[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, imageMessage: { url: res.video[0].thumbnail }, footer: "Rzx Bot", templateButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.video[0].url}` }}] }, { quoted: msg })
       } catch (e) {
        console.log(e)
     }
