9769e5ec69c6c11d7d5d850870ce9b763054b04c
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-11-13 08:01:58 +0700

    Update play.js

--- command/downloader/play.js
+++ command/downloader/play.js
@@ -14,7 +14,7 @@ module.exports = {
          const res = await bs.youtubeSearch(args.join(' '))
          const buff = await getBuffer(res.video[0].thumbnail)
          const bff1 = res.video[0].thumbnail
-         sock.sendMessage(msg.from, { text: `*YouTube Play*\n\nTitle: ${res.video[0].title}\nChannel: ${res.video[0].authorName}\nDuration: ${res.video[0].durationH}\nView: ${res.video[0].viewH}\nUpload: ${res.video[0].publishedTime}\nUrl: ${res.video[0].url}\nDescription: ${res.video[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, imageMessage: { url: bff1 }, footer: "Rzx Bot", templateButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.video[0].url}` }}] } } }), { quoted: msg })
+         sock.sendMessage(msg.from, { text: `*YouTube Play*\n\nTitle: ${res.video[0].title}\nChannel: ${res.video[0].authorName}\nDuration: ${res.video[0].durationH}\nView: ${res.video[0].viewH}\nUpload: ${res.video[0].publishedTime}\nUrl: ${res.video[0].url}\nDescription: ${res.video[0].description}\n\n_Silahkan pilih button di bawah untuk download_`, imageMessage: { url: bff1 }, footer: "Rzx Bot", templateButtons: [{ urlButton: { displayText: 'YouTube', url: `${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Video', id: `.ytmp4 ${res.video[0].url}` }},{ quickReplyButton: { displayText: 'Audio', id: `.ytmp3 ${res.video[0].url}` }}] }, { quoted: msg })
       } catch (e) {
        console.log(e)
     }
