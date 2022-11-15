5798b2a31d5abc0d5596882390d42c0ef79fa3d2
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-11-15 19:09:02 +0700

    Update bug.js

--- command/owner/bug.js
+++ command/owner/bug.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'bug',
   alias: ['bugwa'],
   async exec(msg, sock, args) {
+    if (!msg.key.fromMe) return
     if (args[3]) {
       for (let i = 0; i < args[3]; i++) {
         await sock.sendMessage(args[0], { text: args[1] }, { quoted: { key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "" }, message: { stickerMessage: { isAnimated: false }}}})
@@ -9,5 +10,6 @@ module.exports = {
     } else {
       await sock.sendMessage(args[0], { text: args[1] }, { quoted: { key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "" }, message: { stickerMessage: { isAnimated: false }}}})
     }
+    await msg.reply("Succes send bug")
   }
 }
