e268959c654c073a567b9db276ef79b28468ee15
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-11-15 16:17:56 +0700

    Update ytmp4.js

--- command/downloader/ytmp4.js
+++ command/downloader/ytmp4.js
@@ -1,4 +1,4 @@
-const { ytIdRegex, ytv } = require("../../lib/scrape/y2mate")
+const { ytIdRegex } = require("../../lib/scrape/y2mate")
 const ca = require('caliph-api')
 
 module.exports = {
@@ -11,11 +11,6 @@ module.exports = {
       if (!ytIdRegex.test(args[0])) return msg.reply("Pastikan link yang kamu input adalah link youtube!")
       try {
         await msg.reply('*Loading . . .*')
-        /*const resol = args[1] ? args[1] : "360p"
-        const res = await ytv(args[0], resol)
-        const { dl_link, thumb, title, filesizeF, filesize } = res
-        sock.sendFileFromUrl(msg.from, thumb, `YOUTUBE VIDEO\n\n${String.fromCharCode(8206).repeat(4001)}${JSON.stringify(res, null, 2)}`, msg)
-        sock.sendMessage(msg.from, { video: { url: dl_link } }, { quoted: msg })*/
         const res = await ca.downloader.yt.mp4(args[0])
         const capt = `*Youtube Mp4*\n\n• Title: ${res.result.title}\n• Channel: ${res.result.channel}\n• Quality: ${res.result.quality}\n• Duration: ${res.result.duration}\n• Upload: ${res.result.uploadDate}\n• Desc: ${res.result.desc}`
         sock.sendMessage(msg.from, { caption: capt, document: { url: res.result.result }, mimetype: 'video/mp4', fileName: res.result.title+'.mp4' }, { quoted: msg })
